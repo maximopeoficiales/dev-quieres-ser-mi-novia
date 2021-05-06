4d165d8bf370c85cf1e1d12a59a2590f369d06cd
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-05-05 20:09:09 -0500

    se termino proyecto 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/*.html',
-    './src/**/*.js',
-  ],
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.html',
+      './src/*.html',
+      './src/**/*.js',
+
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
